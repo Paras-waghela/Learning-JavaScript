63017815ee35dc71b1fef58d97eac7d25fac2aad
Paras Waghela <paraswaghela36@gmail.com>
2025-06-17 20:55:14 +0530

    variables in js

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accId = 2839
+let accEmail = "ParasWaghela@gamil.com"
+var accPassword = "12345"
+accCity = "Mumbai"
+
+//accId = 23221
+//can not change the value of constant
+
+/*
+
+var is not used in java script beacuse of block scope and functional scope problem 
+
+*/
+console.log(accId);
+
+console.table([accId,accEmail,accPassword,accCity]);
+
